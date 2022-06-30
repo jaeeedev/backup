5a1744f0b66749d40aacb38ad6a07dd773ccf131
jaeee <72128840+jaeeedev@users.noreply.github.com>
2022-06-30 22:51:51 +0900

    Add files via upload

--- visitbusan/js/app.js
+++ visitbusan/js/app.js
@@ -4,7 +4,23 @@ const btnBg = document.querySelector(".switch_bg");
 
 bestBtn.addEventListener("click", () => {
   btnBg.classList.toggle("active");
+  document.querySelector(".best_busan").classList.toggle("none");
+  document.querySelector(".monthly_busan").classList.toggle("none");
 });
 monthlyBtn.addEventListener("click", () => {
   btnBg.classList.toggle("active");
+  document.querySelector(".best_busan").classList.toggle("none");
+  document.querySelector(".monthly_busan").classList.toggle("none");
+});
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+  },
 });
